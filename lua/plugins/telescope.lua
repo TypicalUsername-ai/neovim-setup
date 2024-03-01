5ae40c85aa41c46e68fdcc27468304f33cc9d0f5
TypicalUsername-ai <mati.domalewski@gmail.com>
2024-03-01 22:42:28 +0100

    extended telescope with treesitter

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,5 +1,7 @@
 return { "https://github.com/nvim-telescope/telescope.nvim", -- @01.03.24 Telescope for file swapping
-		dependencies = { "https://github.com/nvim-lua/plenary.nvim" }, -- 01.03.24 plenary for concurrent util for telescope deps
+		dependencies = { "https://github.com/nvim-lua/plenary.nvim", -- 01.03.24 plenary for concurrent util for telescope deps
+            "https://github.com/nvim-treesitter/nvim-treesitter" -- treesitter as a reuired dep
+        }, 
 	    keys = {
             {"<leader>f", "<cmd>Telescope git_files<cr>", desc = "File finder"},
             {"<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find all files"}
