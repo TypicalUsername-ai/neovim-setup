b0eac37a521f1f8db2c52d1885a44334c3566825
TypicalUsername-ai <mati.domalewski@gmail.com>
2024-03-01 16:28:33 +0100

    added basic editor options

--- init.lua
+++ init.lua
@@ -18,6 +18,23 @@ vim.opt.hlsearch = false -- Disable seeing prev search pattern
 vim.opt.incsearch = true -- show search while typing
 vim.opt.termguicolors = true -- tries to force colored terminal
 
+-- TAB choices to make it 4 spaces
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+-- disable wrapping
+vim.opt.wrap = false
+
+-- SWAP and UNDO commands
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
 -- @01.03.2024 ==> PLUGINS list for lazy nvim
 local plugins = {
 	"https://github.com/nvim-lualine/lualine.nvim", -- @01.03.2024 status line lualine plugin
