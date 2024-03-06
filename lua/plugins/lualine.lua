a1a0deccf27ad012baf025ef77796438d9f6c221
TypicalUsername-ai <mati.domalewski@gmail.com>
2024-03-06 17:41:43 +0100

    lualine modifications

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,17 +1,55 @@
 -- @01.03.2024 ==> PLUGINS list for lazy nvim
 return {
 	"nvim-lualine/lualine.nvim", -- @01.03.2024 status line lualine plugin
+    dependencies = {
+        'nvim-tree/nvim-web-devicons',
+        {
+            'linrongbin16/lsp-progress.nvim',
+            config = true
+        }
+    },
     opts = {
         theme = 'tokyonight',
         sections = {
-            lualine_x = {
+          lualine_c = {
             {
-                require("lazy.status").updates,
-                cond = require("lazy.status").has_updates,
-                color = { fg = "#ff9e64" },
+              "filename",
+              file_status = true,  -- displays file status (readonly status, modified status)
+              path = 1,            -- relative path
+              shorting_target = 40 -- Shortens path to leave 40 space in the window
+            }
+          },
+          lualine_x = {
+            {
+              function()
+                return require("lsp-progress").progress({
+                  max_size = 80,
+                  format = function(messages)
+                    local bufnr = vim.api.nvim_get_current_buf()
+                    local active_clients = vim.lsp.get_active_clients({ bufnr = bufnr })
+                    if #messages > 0 then
+                      return table.concat(messages, " ")
+                    end
+                    local client_names = {}
+                    for _, client in ipairs(active_clients) do
+                      if client and client.name ~= "" then
+                        table.insert(
+                          client_names,
+                          1,
+                          client.name
+                        )
+                      end
+                    end
+                    return table.concat(client_names, "  ")
+                  end,
+                })
+              end,
+              icon = { "", align = "right" },
             },
-        },
-    },
+            "diagnostics",
+          },
+          lualine_y = { "filetype", "encoding", "fileformat" },
+          lualine_z = { "location" },
+        }
     }
-
 }
