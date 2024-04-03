46337130723ac020721729a3fca5f6abd347335f
TypicalUsername-ai <mati.domalewski@gmail.com>
2024-04-03 17:28:39 +0200

    add and setup conform.nvim

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,26 @@
+return {
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local conform = require("conform")
+
+		conform.setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+			},
+			format_on_save = {
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 750,
+			},
+		})
+
+		vim.keymap.set("n", "<leader>F", function()
+			conform.format({
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 750,
+			})
+		end, { desc = "format file" })
+	end,
+}
