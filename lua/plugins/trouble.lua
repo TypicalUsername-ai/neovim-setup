b418f5fc9b217a9f90de6dab56f8cae877a15219
TypicalUsername-ai <mati.domalewski@gmail.com>
2024-04-03 17:28:16 +0200

    add trouble

---
+++ lua/plugins/trouble.lua
@@ -0,0 +1,13 @@
+return {
+	"folke/trouble.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	keys = {
+		{ "<leader>t", "<cmd>TroubleToggle<cr>" },
+	},
+	opts = {
+		position = "right",
+		-- your configuration comes here
+		-- or leave it empty to use the default settings
+		-- refer to the configuration section below
+	},
+}
