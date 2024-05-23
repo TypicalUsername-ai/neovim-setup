7f258bc3c85b4732b6bd93477d1d7fd894787068
TypicalUsername-ai <mati.domalewski@gmail.com>
2024-05-23 14:13:49 +0200

    adding clipboard "Y" yank

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,3 +3,4 @@ vim.g.mapleader = " " -- @01.03.2024 setting LEADER to <space>
 -- Useful normal keymaps
 vim.keymap.set("n", "<leader>l", "<cmd>Lazy<cr>")
 vim.keymap.set("n", "<leader>w", "<cmd>w<cr>")
+vim.keymap.set({ "n", "v" }, "Y", '"+y')
