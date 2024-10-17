09cde197aa71f72ad3ef817d184272c1586f724f
TypicalUsername-ai <mati.domalewski@gmail.com>
2024-10-17 09:44:43 +0000

    lsp: remove mason for LSPs

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,8 +1,6 @@
 return {
 	"neovim/nvim-lspconfig",
 	dependencies = {
-		"williamboman/mason.nvim",
-		"williamboman/mason-lspconfig.nvim",
 	},
 	lazy = false,
 	keys = {
@@ -10,17 +8,6 @@ return {
 		{ "<leader>k", vim.lsp.buf.definition, opts, desc = "Peek definition" },
 	},
 	config = function()
-		require("mason").setup()
-		require("mason-lspconfig").setup_handlers({
-			-- The first entry (without a key) will be the default handler
-			-- and will be called for each installed server that doesn't have
-			-- a dedicated handler.
-			function(server_name) -- default handler (optional)
-				require("lspconfig")[server_name].setup({
-					on_attach = require("lspconfig")[server_name].on_attach,
-					capabilities = require("lspconfig")[server_name].capabilities,
-				})
-			end,
 			-- Next, you can provide a dedicated handler for specific servers.
 			-- For example, a handler override for the `rust_analyzer`:
 			-- ["rust_analyzer"] = function ()
--- lua/plugins/mason.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"williamboman/mason.nvim",
-	opts = {},
-	lazy = false, --make sure mason is working from the get go
-	keys = {
-		{ "<leader>m", "<cmd>Mason<cr>", desc = "Open mason" },
-	},
-}
