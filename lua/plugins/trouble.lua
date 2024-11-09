077d9a3e4a6c79d240d1e2f30ae762fd6af1c4ad
TypicalUsername-ai <mati.domalewski@gmail.com>
2024-11-09 13:28:57 +0000

    trouble: better fixed setup

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -2,10 +2,11 @@ return {
 	"folke/trouble.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	keys = {
-		{ "<leader>t", "<cmd>TroubleToggle<cr>" },
+		{ "<leader>tt", "<cmd>Trouble diagnostics toggle focus=false filter.buf=0<cr>", desc = "Open trouble for current buffer" },
+		{ "<leader>tq", "<cmd>Trouble qflist toggle<cr>", desc = "Quickfix List (Trouble)" },
 	},
 	opts = {
-		-- position = "right",
+		position = "right",
 		-- your configuration comes here
 		-- or leave it empty to use the default settings
 		-- refer to the configuration section below
