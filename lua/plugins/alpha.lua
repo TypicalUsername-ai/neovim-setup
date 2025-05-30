c7399a2c1b166fcc11569d2dba347dbe9315c08d
TypicalUsername-ai <mati.domalewski@gmail.com>
2025-05-30 19:48:47 +0000

    fixup! new: add alpha-nvim welcome screen
    adding the missing return statement

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,6 +1,6 @@
-{
-    'goolord/alpha-nvim',
-    config = function ()
-        require'alpha'.setup(require'alpha.themes.dashboard'.config)
-    end
-};
+return {
+	"goolord/alpha-nvim",
+	config = function()
+		require("alpha").setup(require("alpha.themes.dashboard").config)
+	end,
+}
